1655c4b300e787990ae336d12dfd686bf7116046
karen okasaki <oks.karen@gmail.com>
2023-04-29 09:44:45 -0300

    sort

---
+++ modulo2/advenced/sort.js
@@ -0,0 +1,176 @@
+//MÉTODOS MÚTAVEIS
+//alterar a minha array original
+
+//sort()
+//b  //a
+const numbers = [22, 23, 99, 68, 1, 0, 9, 112, 223, 64, 18];
+
+numbers.sort((a, b) => {
+  // a - b = MAIOR QUE ZERO -> a é maior que b
+  // a - b = MENOR QUE ZERO -> b é menor que a
+  // a - b = 0 -> os numeros são iguais
+
+  console.log("a:", a, "-", "b:", b, "=", a - b);
+  //a - b ->forma crescente
+  //b -a  -> forma descrecente
+  return a - b;
+});
+
+console.log(numbers.reverse());
+
+console.log(numbers);
+
+const students = [
+  {
+    name: "Tony Parker",
+    firstProject: 80,
+    secondProject: 75,
+    finalExam: 90,
+  },
+  {
+    name: "Marc Barchini",
+    firstProject: 84,
+    secondProject: 65,
+    finalExam: 65,
+  },
+  {
+    name: "Claudia Lopez",
+    firstProject: 45,
+    secondProject: 95,
+    finalExam: 99,
+  },
+  {
+    name: "Alvaro Briattore",
+    firstProject: 82,
+    secondProject: 92,
+    finalExam: 70,
+  },
+  {
+    name: "Isabel Ortega",
+    firstProject: 90,
+    secondProject: 32,
+    finalExam: 85,
+  },
+  {
+    name: "Francisco Martinez",
+    firstProject: 90,
+    secondProject: 55,
+    finalExam: 78,
+  },
+  {
+    name: "Jorge Carrillo",
+    firstProject: 83,
+    secondProject: 77,
+    finalExam: 90,
+  },
+  {
+    name: "Miguel López",
+    firstProject: 80,
+    secondProject: 75,
+    finalExam: 75,
+  },
+  {
+    name: "Carolina Perez",
+    firstProject: 85,
+    secondProject: 72,
+    finalExam: 67,
+  },
+  {
+    name: "Ruben Pardo",
+    firstProject: 89,
+    secondProject: 72,
+    finalExam: 65,
+  },
+];
+
+//spread -> ESPALHA
+// arrays e OBJETOS
+
+//criar um clone
+
+//memoria do seu computador students -> 20
+
+const sortedArray = [...students];
+console.log(sortedArray);
+
+//ORDENAR A ARRAY DA MAIOR NOTA DO FINALEXAM PARA MENOR NOTA
+sortedArray.sort((a, b) => {
+  return b.finalExam - a.finalExam;
+});
+console.log(students);
+console.log(sortedArray);
+
+const places = [
+  {
+    title: "Awesome Suite 20' away from la Rambla",
+    price: 200,
+    type: "Private Room",
+    pool: true,
+    garage: false,
+  },
+  {
+    title: "Private apartment",
+    price: 190,
+    type: "Entire Place",
+    pool: true,
+    garage: true,
+  },
+  {
+    title: "Apartment with awesome views",
+    price: 400,
+    type: "Entire Place",
+    pool: false,
+    garage: false,
+  },
+  {
+    title: "Apartment in la Rambla",
+    price: 150,
+    type: "Private Room",
+    pool: false,
+    garage: true,
+  },
+  {
+    title: "Comfortable place in Barcelona´s center",
+    price: 390,
+    type: "Entire place",
+    pool: true,
+    garage: true,
+  },
+  {
+    title: "Room near Sagrada Familia",
+    price: 170,
+    type: "Private Room",
+    pool: false,
+    garage: false,
+  },
+  {
+    title: "Great house next to Camp Nou",
+    price: 140,
+    type: "Entire place",
+    pool: true,
+    garage: true,
+  },
+  {
+    title: "New apartment with 2 beds",
+    price: 2000,
+    type: "Entire place",
+    pool: false,
+    garage: true,
+  },
+  {
+    title: "Awesome Suite",
+    price: 230,
+    type: "Private Room",
+    pool: false,
+    garage: false,
+  },
+  {
+    title: "Apartment 10' from la Rambla",
+    price: 930,
+    type: "Entire place",
+    pool: true,
+    garage: true,
+  },
+];
+
+//SORTEAR A ARRAY DO MENOR PREÇO PARA O MAIOR PREÇO
