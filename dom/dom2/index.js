f946375c75b5b89268ad22141e538495eb5e4075
karen okasaki <oks.karen@gmail.com>
2023-05-02 21:11:49 -0300

    cal

--- dom/dom2/index.js
+++ dom/dom2/index.js
@@ -1,8 +1,16 @@
 const startBtn = document.querySelector("#startBtn");
+
+const inputName = document.querySelector("#inputName");
+const spanName = document.querySelector("#spanName");
+
 const divHome = document.querySelector("#divHome");
 const divBoard = document.querySelector("#divBoard");
-const inputName = document.querySelector("#inputName")
-// 
+
+const num1 = document.querySelector("#num1");
+const num2 = document.querySelector("#num2");
+const op = document.querySelector("#op");
+const resultButton = document.querySelector("#resultButton");
+const result = document.querySelector("#result");
 
 //adicionar um eventListener no botão começar.
 startBtn.addEventListener("click", () => {
@@ -12,6 +20,22 @@ startBtn.addEventListener("click", () => {
 
   // mostrar o board
   divBoard.classList.remove("hide");
+
+  //capturar o nome do jogador
+  spanName.innerText = inputName.value;
 });
 
+//quando o usuário clicar no RESULTBUTTON
+//fazer a conta ou seja -> num1 num2 e operação matemática
 
+resultButton.addEventListener("click", () => {
+  console.log("Resultado");
+  console.log(op.value);
+
+  if (op.value === "+") {
+    result.innerText = Number(num1.value) + Number(num2.value);
+  }
+
+  console.log(num1.value);
+  console.log(num2.value);
+});
